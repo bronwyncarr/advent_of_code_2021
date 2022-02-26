356fabbfccf2a50fff7051d5f53d692725c9bec6
Bronwyn Carr <bronwynacarr@gmail.com>
2022-02-26 21:13:25 +1100

    day two tests updated

--- day2/day_2.js
+++ day2/day_2.js
@@ -8,11 +8,43 @@ let input = fs.readFileSync(pathToFile, "utf-8");
 const formatData = (input) => input.split("\n");
 let inputArray = formatData(input);
 
-// Part One
-const calculatePosition = (inputArray) => {
+const partOneInstructions = (currentPosition, direction, amount) => {
+  switch (direction) {
+    case "up":
+      currentPosition["y"] -= amount;
+      break;
+    case "down":
+      currentPosition["y"] += amount;
+      break;
+    case "forward":
+      currentPosition["x"] += amount;
+      break;
+  }
+  return currentPosition;
+};
+
+const partTwoInstructions = (currentPosition, direction, amount) => {
+  switch (direction) {
+    case "up":
+      currentPosition["z"] -= amount;
+      break;
+    case "down":
+      currentPosition["z"] += amount;
+      break;
+    case "forward":
+      currentPosition["x"] += amount;
+      currentPosition["y"] =
+        currentPosition["y"] + currentPosition["z"] * amount;
+      break;
+  }
+  return currentPosition;
+};
+
+const calculatePosition = (inputArray, callback) => {
   let currentPosition = {
     x: 0,
     y: 0,
+    z: 0,
   };
 
   const pattern = /(forward|up|down) (\d+)/i;
@@ -21,35 +53,40 @@ const calculatePosition = (inputArray) => {
     let commandMatch = pattern.exec(command);
     let [, direction, amount] = commandMatch;
     amount = parseInt(amount);
-
-    switch (direction) {
-      case "up":
-        currentPosition["y"] -= amount;
-        break;
-      case "down":
-        currentPosition["y"] += amount;
-        break;
-      case "forward":
-        currentPosition["x"] += amount;
-        break;
-    }
+    currentPosittion = callback(currentPosition, direction, amount);
   });
 
   return currentPosition;
 };
 
-const finalPosition = calculatePosition(inputArray);
-console.log(
-  `The final position is ${finalPosition["y"]} height and ${finalPosition["x"]} forward`
-);
-
 const multiplyPosition = (finalPosition) =>
   finalPosition["x"] * finalPosition["y"];
 
-console.log(`multiplied value is: ${multiplyPosition(finalPosition)}`);
+// Part One
+const finalPositionPartOne = calculatePosition(inputArray, partOneInstructions);
+console.log(
+  `The final position in PART ONE is ${finalPositionPartOne["y"]} height and ${finalPositionPartOne["x"]} forward.`
+);
+
+console.log(
+  `Multiplied value in PART ONE is: ${multiplyPosition(finalPositionPartOne)}.`
+);
+
+// Part Two
+const finalPositionPartTwo = calculatePosition(inputArray, partTwoInstructions);
+
+console.log(
+  `The final position for PART TWO IS ${finalPositionPartTwo["y"]} height and ${finalPositionPartTwo["x"]} forward.`
+);
+console.log(
+  `Multiplied value in PART TWO is: ${multiplyPosition(finalPositionPartTwo)}.
+  }`
+);
 
 // export for testing
 module.exports = {
   calculatePosition,
+  partOneInstructions,
+  partTwoInstructions,
   multiplyPosition,
 };
--- day2/day_2.test.js
+++ day2/day_2.test.js
@@ -1,4 +1,10 @@
-const { calculatePosition, multiplyPosition } = require("./day_2.js");
+const {
+  calculatePosition,
+  partOneInstructions,
+  partTwoInstructions,
+  multiplyPosition,
+} = require("./day_2.js");
+
 const mockData = [
   "forward 5",
   "down 5",
@@ -8,21 +14,63 @@ const mockData = [
   "forward 2",
 ];
 
-const mockResultPosition = { x: 15, y: 10 };
+let mockStartPosition = { x: 0, y: 0, z: 0 };
+
+beforeEach(() => {
+  mockStartPosition = { x: 0, y: 0, z: 0 };
+});
+
+describe("part one instructions", () => {
+  it("will change the position ", () => {
+    const mockPartOneResultPosition = { x: 5, y: 0, z: 0 };
+
+    const currentPosition = partOneInstructions(
+      mockStartPosition,
+      "forward",
+      5
+    );
+
+    expect(currentPosition).toEqual(mockPartOneResultPosition);
+  });
+});
+
+describe("part two instructions", () => {
+  it("will change the position ", () => {
+    const mockPartTwoResultPosition = { x: 5, y: 0, z: 0 };
+
+    const currentPosition = partOneInstructions(
+      mockStartPosition,
+      "forward",
+      5
+    );
+
+    expect(currentPosition).toEqual(mockPartTwoResultPosition);
+  });
+});
 
 describe("calculate position", () => {
-  it("returns the x and y axis position", () => {
-    const currentPosition = calculatePosition(mockData);
+  it("returns the position with part one instructions", () => {
+    const mockPartOneResultPosition = { x: 15, y: 10, z: 0 };
 
-    expect(currentPosition).toEqual(mockResultPosition);
+    const currentPosition = calculatePosition(mockData, partOneInstructions);
+
+    expect(currentPosition).toEqual(mockPartOneResultPosition);
+  });
+
+  it("returns the position with part one instructions", () => {
+    const mockPartTwoResultPosition = { x: 15, y: 60, z: 10 };
+
+    const currentPosition = calculatePosition(mockData, partTwoInstructions);
+
+    expect(currentPosition).toEqual(mockPartTwoResultPosition);
   });
 });
 
-describe("multiply position", () => {
-  it("returns the multiplication of the x and y axis position", () => {
-    const currentPosition = calculatePosition(mockData);
-    const multiplyPositionResult = multiplyPosition(currentPosition);
+// describe("multiply position", () => {
+//   it("returns the multiplication of the x and y axis position", () => {
+//     const currentPosition = calculatePosition(mockData);
+//     const multiplyPositionResult = multiplyPosition(currentPosition);
 
-    expect(multiplyPositionResult).toBe(150);
-  });
-});
+//     expect(multiplyPositionResult).toBe(150);
+//   });
+// });
